aa1baa9c181e1a4b0f48d13775b17596e658b00b
josanandi <andreea.josan@gmail.com>
2018-07-25 20:23:49 +0200

    b2

--- src/Andi.java
+++ src/Andi.java
@@ -1,10 +1,10 @@
 public class Andi {
     public static void main(String[] args) {
-        count();
+        count(34);
 
     }
 
-    public static void count(){
-        System.out.println("I am 34 years");
+    public static void count(Integer b){
+        System.out.println("I am " + b);
     }
 }
