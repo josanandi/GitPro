571698a6111be620ee3b3b2929331a393c7f971a
josanandi <andreea.josan@gmail.com>
2018-07-25 20:45:16 +0200

    b3

--- src/Eniko.java
+++ src/Eniko.java
@@ -1,5 +1,6 @@
 public class Eniko {
     private String teamName = "Emerald Unicorn Glitter";
+    private String name = "Java";
 
     public void cheer(String name) {
         System.out.println(name);
