84be7baa2509dbff63c9391083b63a6caab19072
EnikoKancsar <enikokancsar@gmail.com>
2018-07-26 16:42:08 +0200

    c5.3

--- src/Eniko.java
+++ src/Eniko.java
@@ -1,6 +1,7 @@
 public class Eniko {
     private String teamName = "Emerald Unicorn Glitter";
     private String name = "Java";
+    private String random = "Just a random variable";
 
     public void cheer(String name, String teamName) {
         System.out.println(name + " " + teamName);
