c43fd2684776565afcaf8d3abd92b61d2f588a38
EnikoKancsar <enikokancsar@gmail.com>
2018-07-27 17:44:40 +0200

    eniko0727

--- src/Kriszga.java
+++ src/Kriszga.java
@@ -2,4 +2,8 @@ public class Kriszga {
     public void future(String something) {
         System.out.println(something);
     }
+
+    public void eniko0727() {
+        System.out.println("mukodj");
+    }
 }
